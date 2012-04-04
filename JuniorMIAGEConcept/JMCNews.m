77646dbea6d0897c8f782e5397cf2f2e105ecaa7
Hugo Vicard <h.vicard@gmail.com>
2012-04-04 10:47:43 +0200

    no message

---
+++ JuniorMIAGEConcept/JMCNews.m
@@ -0,0 +1,43 @@
+//
+//  JMCNews.m
+//  JuniorMIAGEConcept
+//
+//  Created by Hugo Vicard on 04/04/12.
+//  Copyright (c) 2012 Hugo Vicard. All rights reserved.
+//
+
+#import "JMCNews.h"
+
+@implementation JMCNews
+
+
+@synthesize title;
+@synthesize pubDate;
+@synthesize author;
+@synthesize category;
+@synthesize description;
+
+- (id)initWithName:(NSString *)newTitle 
+           pubDate:(NSString *)newPubDate 
+            author:(NSString *)newAuthor 
+          category:(NSString *)newCategory 
+       description:(NSString *)newDescription{
+    
+    if ((self = [super init])) {
+        self.title = newTitle;
+        self.pubDate = newPubDate;
+        self.author = newAuthor;
+        self.category = newCategory;
+        self.description = newDescription;
+    }    
+    return self;
+    
+}
+
+- (void) dealloc {
+    self.title = nil;    
+    [super dealloc];
+}
+
+
+@end
